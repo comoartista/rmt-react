96595a7a9111d2cd38e43943e7ca173ac026286e
Oksana Melnyk <oks.melnuk@gmail.com>
2024-03-22 13:47:14 +0100

    added key

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,50 @@
+import { useState } from "react";
+import Background from "./Background";
+import Container from "./Container";
+import Footer from "./Footer";
+import Header, { HeaderTop } from "./Header";
+import BookmarksButton from "./BookmarksButton";
+import Logo from "./Logo";
+import SearchForm from "./SearchForm";
+import Sidebar, { SidebarTop } from "./Sidebar";
+import JobItemContent from "./JobItemContent";
+import ResultsCount from "./ResultsCount";
+import Sortingcontrols from "./SortingControls";
+import JobList from "./JobList";
+import Paginationcontrols from "./PaginationControls";
+import { useJobItems } from "../lib/hooks";
+
 function App() {
-  return <>Hello!</>;
+  const [searchText, setSearchText] = useState("");
+  const [ jobItems, isLoading ] = useJobItems(searchText);
+
+  return (
+    <>
+      <Background />
+
+      <Header>
+        <HeaderTop>
+          <Logo />
+          <BookmarksButton />
+        </HeaderTop>
+        <SearchForm searchText={searchText} setSearchText={setSearchText} />
+      </Header>
+
+      <Container>
+        <Sidebar>
+          <SidebarTop>
+            <ResultsCount />
+            <Sortingcontrols />
+          </SidebarTop>
+          <JobList jobItems={jobItems} isLoading={isLoading} />
+          <Paginationcontrols />
+        </Sidebar>
+        <JobItemContent />
+      </Container>
+
+      <Footer />
+    </>
+  );
 }
 
 export default App;
---
+++ src/lib/hooks.ts
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "react";
+import { JobItem } from "./types";
+
+export function useJobItems(searchText: string) {
+  const [jobItems, setJobItems] = useState<JobItem[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const jobItemsSliced = jobItems.slice(0, 7);
+
+  useEffect(() => {
+    if (!searchText) {
+      return;
+    }
+
+    setIsLoading(true);
+
+    const fetchData = async () => {
+      const response = await fetch(
+        `https://bytegrad.com/course-assets/projects/rmtdev/api/data?search=${searchText}` +
+          searchText
+      );
+
+      const data = await response.json();
+      setIsLoading(false);
+
+      setJobItems(data.jobItems);
+    };
+
+    fetchData();
+  }, [searchText]);
+
+  return [jobItemsSliced, isLoading] as const;
+}
